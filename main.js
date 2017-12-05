9ca4782a389b08dfdf4e63fce76709f3422e6336
Michael <learicist@gmail.com>
2017-12-04 19:24:12 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-  $("h1").click(function() {
+  $("h1").on("click", function() {
     $(this).effect("bounce", { times: 4 }, 1500);
   });
-  $(".img-responsive").click(function() {
+  $(".img-responsive").on("click", function() {
     $(this).effect("slide");
   });
+  
 });
